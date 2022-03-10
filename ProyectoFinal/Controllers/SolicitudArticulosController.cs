e73920d24e2a9a51465612a97e29b91daa8d780c
Michael Jeffrey <michaeljeffrey_rodriguez@bhdleon.com.do>
2022-03-10 08:52:32 -0400

    Add project files.

---
+++ ProyectoFinal/Controllers/SolicitudArticulosController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoFinal.Models;
+
+namespace ProyectoFinal.Controllers
+{
+    public class SolicitudArticulosController : Controller
+    {
+        private ProjectDbContext db = new ProjectDbContext();
+
+        // GET: SolicitudArticulos
+        public ActionResult Index()
+        {
+            return View(db.SolicitudArticulos.ToList());
+        }
+
+        // GET: SolicitudArticulos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SolicitudArticulo solicitudArticulo = db.SolicitudArticulos.Find(id);
+            if (solicitudArticulo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(solicitudArticulo);
+        }
+
+        // GET: SolicitudArticulos/Create
+        [Authorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SolicitudArticulos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Empleado,FechaSolicitud,Articulo,Cantidad,UnidadMedida,Estado")] SolicitudArticulo solicitudArticulo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SolicitudArticulos.Add(solicitudArticulo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(solicitudArticulo);
+        }
+
+        // GET: SolicitudArticulos/Edit/5
+        [Authorize]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SolicitudArticulo solicitudArticulo = db.SolicitudArticulos.Find(id);
+            if (solicitudArticulo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(solicitudArticulo);
+        }
+
+        // POST: SolicitudArticulos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Empleado,FechaSolicitud,Articulo,Cantidad,UnidadMedida,Estado")] SolicitudArticulo solicitudArticulo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(solicitudArticulo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(solicitudArticulo);
+        }
+
+        // GET: SolicitudArticulos/Delete/5
+        [Authorize(Roles = "Administrador")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SolicitudArticulo solicitudArticulo = db.SolicitudArticulos.Find(id);
+            if (solicitudArticulo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(solicitudArticulo);
+        }
+
+        // POST: SolicitudArticulos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SolicitudArticulo solicitudArticulo = db.SolicitudArticulos.Find(id);
+            db.SolicitudArticulos.Remove(solicitudArticulo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ProyectoFinal/Models/SolicitudArticulo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFinal.Models
+{
+    public class SolicitudArticulo
+    {
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("Empleado Solicitante")]
+        [Required]
+        public string Empleado { get; set; }
+        [DisplayName("Fecha de solicitud")]
+        public DateTime FechaSolicitud { get; set; }
+        [Required]
+        public string Articulo { get; set; }
+        public int Cantidad { get; set; }
+        [DisplayName("Unidad de Medida")]
+        public string UnidadMedida { get; set; }
+        public bool Estado { get; set; }
+    }
+
+    public class ProjectDbContext : ApplicationDbContext
+    {
+        public DbSet<SolicitudArticulo> SolicitudArticulos { get; set; }
+    }
+}
